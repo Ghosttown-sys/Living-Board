8423510ecabb04311d3f506bc18ee451220165c7
ZhengYiHu <zhengyi.hu.98@gmail.com>
2024-05-27 16:11:35 -0400

    change draggable cards into 2d nodes

--- Game/Core/Card/Core_Card.gd
+++ Game/Core/Card/Core_Card.gd
@@ -2,24 +2,25 @@ class_name Card_UI
 extends Control
 @onready var hand = get_parent() 
 @export var rotation_curve : Curve;
-var original_index := 0
 var parent: Control
 var tween: Tween
-var rotation_tween : Tween
 var disabled := false
 var card_anime = null
-var card_rotation :float;
+
+var draggable_card = preload("res://Game/Core/cards/draggable_card.tscn")
+var draggable_card_instance;
 
 var dragging :
 	get: 
-		if $Draggable_Item == null:
+		if draggable_card_instance == null:
 			return false
 		else:
-			return $Draggable_Item.dragging
+			return draggable_card_instance.dragging
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	card_anime = get_node("card_anime")
+	Events.on_card_returned_to_hand.connect(_on_draggable_card_released)
 
 func animate_to_position(new_position: Vector2, duration: float, new_rotation: float) -> void:
 	tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
@@ -33,3 +34,17 @@ func _on_control_mouse_entered():
 
 func _on_control_mouse_exited():
 	card_anime.play_backwards("focus")
+
+# When clicked, create a new 2dNode object to act as the draggable object
+func _on_texture_rect_gui_input(event):
+	if Input.is_action_just_pressed("select_card"):
+		draggable_card_instance = draggable_card.instantiate()
+		hand.draggable_items_parent.add_child(draggable_card_instance)
+		draggable_card_instance.in_hand_card = self
+		draggable_card_instance.position = position
+		draggable_card_instance.start_dragging()
+		visible = false
+
+func _on_draggable_card_released(card):
+	if card == self: 
+		visible = true
--- Game/Core/utils/lerp_follow.gd
+++ Game/Core/utils/lerp_follow.gd
@@ -1,17 +1,16 @@
-extends Control
+extends Node2D
 
-@export var to_follow : Control
-@export var follow_position_speed : float;
-@export var follow_rotation_speed : float;
+@export var to_follow : Node2D
+@export var follow_position_speed : float = 8
+@export var follow_rotation_speed : float = 4
 
 var rotationDelta : Vector2
 
 func  _ready():
 	global_position = to_follow.global_position;
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	position = position.lerp(to_follow.position, delta * follow_position_speed)
 	var movement = position - to_follow.position
 	rotationDelta = rotationDelta.lerp(movement , delta * follow_rotation_speed)
-	rotation_degrees = -clamp(rotationDelta.x,-60,60)
+	rotation_degrees = -clamp(rotationDelta.x,-60,60) + to_follow.rotation_degrees
