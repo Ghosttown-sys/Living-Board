483a69a6157610c301cca12487aa98009785e685
ZhengYiHu <zhengyi.hu.98@gmail.com>
2024-05-31 01:33:21 -0400

    fix: audio fading

--- Global/audio_manager.gd
+++ Global/audio_manager.gd
@@ -11,16 +11,19 @@ Tracklist:
 	0 - bg_music
 	1 - combat_music
 """
-var current_track : int
+var current_track : int = -1
 
 func play_music(track_to_play):
 	
 	if track_to_play >= tracks.size() || track_to_play < 0:
 		printerr("Invalid track index")
 		return
-	
+	if track_to_play == current_track:
+		return
 	tracks[track_to_play].play()
 	var tween = get_tree().create_tween()
-	tween.tween_property(tracks[current_track], "volume_db", 0, 0.8)
-	tween.tween_property(tracks[track_to_play], "volume_db", 1, 0.8)
+	tween.tween_property(tracks[track_to_play], "volume_db", 0, 1)
+	await tween.tween_property(tracks[current_track], "volume_db", -80, 1)
+	tracks[current_track].stop()
 	current_track = track_to_play
+	
