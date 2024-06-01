2c155b7d8dd6022d9603d01418c08e4eab6cff00
Ghosttown-sys <121886253+Ghosttown-sys@users.noreply.github.com>
2024-06-01 23:40:34 +0530

    Minor UI updates

--- Game/Scenes/Run/Run.gd
+++ Game/Scenes/Run/Run.gd
@@ -1,9 +1,14 @@
 extends Node2D
-@onready var debug = $Debug
 @onready var board = $Board
+@onready var turn_controler = $Turn_Controler
 
 const COMBAT = preload("res://Game/Scenes/Combat/combat.tscn")
 @onready var camera :Camera2D= $Camera
+
+@onready var hp = $Top_Bar/Menu/Profile/Profile/Stats/HP
+@onready var sanity = $Top_Bar/Menu/Profile/Profile/Stats/Sanity
+@onready var turn_counter = $Top_Bar/Menu/Turn/Turn_Counter
+@onready var phase = $Top_Bar/Menu/Game_State/Phase
 
 
 func _on_button_pressed():
@@ -14,6 +19,6 @@ func _on_button_pressed():
 
 
 func toggle_visiblity():
-	debug.hide()
+	turn_controler.hide()
 	board.visible =false
 	board.buttons.hide()
